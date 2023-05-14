2037cf7fdb52ef3709126480da22f02ad2edc91c
amirn003 <amir.naar@gmail.com>
2023-05-15 00:02:59 +0200

    add dealership and review endpoints

--- functions/sample/python/review.py
+++ functions/sample/python/review.py
@@ -1,7 +1,19 @@
+"""
+This module contains functions for posting reviews for a dealership using IBM Cloud Functions.
+"""
+
 from ibmcloudant.cloudant_v1 import CloudantV1
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 
 def main(params):
+    """Handle the main logic of posting reviews for a dealership.
+
+    Args:
+        params (dict): Dictionary containing input parameters.
+
+    Returns:
+        dict: Result of the review posting operation.
+    """
     url = "https://39c5ae9d-7f45-4c94-bb02-22a7ce000df0-bluemix.cloudantnosqldb.appdomain.cloud"
     authenticator = IAMAuthenticator("_jmHxOs2-RLKmlfJ2JjQRzEzZ1mnl5bm3SS15Zv66N6d")
     service = CloudantV1(authenticator=authenticator)
@@ -46,7 +58,7 @@ def main(params):
                 }
             }
             return result
-        except ValueError as e:
+        except ValueError as exception:
             result = {
                 'statusCode': 500,
                 'message': 'Failed to post review'
