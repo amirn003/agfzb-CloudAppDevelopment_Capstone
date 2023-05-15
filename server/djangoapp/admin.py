5568cdc4ad65e3fdb9a37b00d992ad296cb56482
amirn003 <amir.naar@gmail.com>
2023-05-16 00:13:18 +0200

    add CarMake and CarModel models

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,13 +1,21 @@
 from django.contrib import admin
-# from .models import related models
-
+from .models import CarMake, CarModel
 
 # Register your models here.
 
 # CarModelInline class
+class CarModelInline(admin.TabularInline):
+    model = CarModel
+    extra = 1  # Set the number of empty extra forms to display
+    fields = ['name', 'dealer_id', 'type', 'year']  # Specify the fields to display
+
 
 # CarModelAdmin class
+@admin.register(CarModel)
+class CarModelAdmin(admin.ModelAdmin):
+    pass
 
 # CarMakeAdmin class with CarModelInline
-
-# Register models here
+@admin.register(CarMake)
+class CarMakeAdmin(admin.ModelAdmin):
+    inlines = [CarModelInline]
--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,6 +1,13 @@
 from django.db import models
 from django.utils.timezone import now
+try:
+    from django.db import models
+except Exception:
+    print("There was an error loading django modules. Do you have django installed?")
+    sys.exit()
 
+from django.conf import settings
+import uuid
 
 # Create your models here.
 
@@ -9,6 +16,13 @@ from django.utils.timezone import now
 # - Description
 # - Any other fields you would like to include in car make model
 # - __str__ method to print a car make object
+class CarMake(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+    comment = models.CharField(max_length=150)
+
+    def __str__(self):
+        return self.name
 
 
 # <HINT> Create a Car Model model `class CarModel(models.Model):`:
@@ -19,6 +33,21 @@ from django.utils.timezone import now
 # - Year (DateField)
 # - Any other fields you would like to include in car model
 # - __str__ method to print a car make object
+class CarModel(models.Model):
+    car_make = models.ForeignKey('CarMake', on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    dealer_id = models.CharField(max_length=100)
+    TYPE_CHOICES = [
+        ('SEDAN', 'Sedan'),
+        ('SUV', 'SUV'),
+        ('WAGON', 'Wagon'),
+        ('BERLINE', 'Berline'),
+    ]
+    type = models.CharField(max_length=20, choices=TYPE_CHOICES)
+    year = models.DateField()
+
+    def __str__(self):
+        return self.name
 
 
 # <HINT> Create a plain Python class `CarDealer` to hold dealer data
