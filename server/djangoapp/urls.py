06bdf45c641a4a82b46f2f53296c44ff988f4ef7
amirn003 <amir.naar@gmail.com>
2023-05-16 23:50:36 +0200

    add GET dealership restapi, views, url

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     # view refers to the view function
     # name the URL
     path('view/', views.static_page, name='my-view'),
+    path('index/', views.get_index, name='index'),
     # path for about view
     path('about/', views.about, name='about'),
     # path for contact us view
