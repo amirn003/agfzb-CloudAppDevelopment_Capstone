418effc80afe6b51b51eaf4a925b61fe4e04b9fd
amirn003 <amir.naar@gmail.com>
2023-05-18 22:36:44 +0200

    add get reviews by id cloudant function

--- functions/sample/python/get_reviews_cloudant.py
+++ functions/sample/python/get_reviews_cloudant.py
@@ -1,11 +1,18 @@
+'''
+Module to get reviews by dealership in CloudantV1
+'''
 import sys
 from ibmcloudant.cloudant_v1 import CloudantV1
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 
 def main(dict):
+    '''
+    Main return json with the content from the reviews
+    '''
     authenticator = IAMAuthenticator("_jmHxOs2-RLKmlfJ2JjQRzEzZ1mnl5bm3SS15Zv66N6d")
     service = CloudantV1(authenticator=authenticator)
-    service.set_service_url("https://39c5ae9d-7f45-4c94-bb02-22a7ce000df0-bluemix.cloudantnosqldb.appdomain.cloud")
+    url="https://39c5ae9d-7f45-4c94-bb02-22a7ce000df0-bluemix.cloudantnosqldb.appdomain.cloud"
+    service.set_service_url(url)
     response = service.post_find(
                 db='reviews',
                 selector={'dealership': int(dict['id'])},
